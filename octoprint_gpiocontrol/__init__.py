8d59a84d196143e752942d849d8d589a78651cf9
Jaidan Brown <jaidanbrown1999@gmail.com>
2024-01-06 15:33:30 +0000

    Update __init__.py
    New code

--- octoprint_gpiocontrol/__init__.py
+++ octoprint_gpiocontrol/__init__.py
@@ -4,8 +4,8 @@ from octoprint.server import user_permission
 
 import octoprint.plugin
 import flask
-import RPi.GPIO as GPIO
-
+#import RPi.GPIO as GPIO
+from gpiozero import LED
 
 class GpioControlPlugin(
     octoprint.plugin.StartupPlugin,
@@ -15,19 +15,10 @@ class GpioControlPlugin(
     octoprint.plugin.SimpleApiPlugin,
     octoprint.plugin.RestartNeedingPlugin,
 ):
-    mode = None
 
-    def on_startup(self, *args, **kwargs):
-        GPIO.setwarnings(False)
-
-        self.mode = GPIO.getmode()
-
-        if self.mode is None:
-            self.mode = GPIO.BCM
-            GPIO.setmode(self.mode)
-
-        self._logger.info("Detected GPIO mode: {}".format(self.mode))
-
+    def __init__(self):
+        self.devices = {}  # Initialize the devices dictionary
+        
     def get_template_configs(self):
         return [
             dict(type="settings", custom_bindings=True),
@@ -49,124 +40,49 @@ class GpioControlPlugin(
         return dict(gpio_configurations=[])
 
     def on_settings_save(self, data):
-        for configuration in self._settings.get(["gpio_configurations"]):
-            self._logger.info(
-                "Cleaned GPIO{}: {},{} ({})".format(
-                    configuration["pin"],
-                    configuration["active_mode"],
-                    configuration["default_state"],
-                    configuration["name"],
-                )
-            )
+        super(GpioControlPlugin, self).on_settings_save(data)  # Save settings with parent method
 
-            pin = self.get_pin_number(int(configuration["pin"]))
+        new_configurations = self._settings.get(["gpio_configurations"])
+        updated_devices = {}
 
-            if pin > 0:
-                GPIO.cleanup(pin)
+        for configuration in new_configurations:
+            pin = int(configuration["pin"])
 
-        octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+            # Check if this pin already has a device associated
+            if pin in self.devices:
+                # If device exists, you can update settings or recreate it
+                # For simplicity, we'll just recreate it here
+                self.devices[pin].close()  # Properly close the existing device
 
-        for configuration in self._settings.get(["gpio_configurations"]):
-            self._logger.info(
-                "Reconfigured GPIO{}: {},{} ({})".format(
-                    configuration["pin"],
-                    configuration["active_mode"],
-                    configuration["default_state"],
-                    configuration["name"],
-                )
-            )
+            # Create a new device for this pin
+            # Replace 'LED' with the appropriate class for your device
+            updated_devices[pin] = LED(pin)
 
-            pin = self.get_pin_number(int(configuration["pin"]))
+            # Optionally, set the initial state of the device
+            # Example: turning on/off the LED based on some configuration parameter
+            if configuration.get("default_state") == "default_on":
+                updated_devices[pin].on()
+            elif configuration.get("default_state") == "default_off":
+                updated_devices[pin].off()
 
-            if pin > 0:
-                GPIO.setup(pin, GPIO.OUT)
-
-                if configuration["active_mode"] == "active_low":
-                    if configuration["default_state"] == "default_on":
-                        GPIO.output(pin, GPIO.LOW)
-                    elif configuration["default_state"] == "default_off":
-                        GPIO.output(pin, GPIO.HIGH)
-                elif configuration["active_mode"] == "active_high":
-                    if configuration["default_state"] == "default_on":
-                        GPIO.output(pin, GPIO.HIGH)
-                    elif configuration["default_state"] == "default_off":
-                        GPIO.output(pin, GPIO.LOW)
+        # Update the devices dictionary with the new configurations
+        self.devices = updated_devices
 
     def on_after_startup(self):
+        self.devices = {}
         for configuration in self._settings.get(["gpio_configurations"]):
-            self._logger.info(
-                "Configured GPIO{}: {},{} ({})".format(
-                    configuration["pin"],
-                    configuration["active_mode"],
-                    configuration["default_state"],
-                    configuration["name"],
-                )
-            )
-
-            pin = self.get_pin_number(int(configuration["pin"]))
-
-            if pin != -1:
-                GPIO.setup(pin, GPIO.OUT)
-
-                if configuration["active_mode"] == "active_low":
-                    if configuration["default_state"] == "default_on":
-                        GPIO.output(pin, GPIO.LOW)
-                    elif configuration["default_state"] == "default_off":
-                        GPIO.output(pin, GPIO.HIGH)
-                elif configuration["active_mode"] == "active_high":
-                    if configuration["default_state"] == "default_on":
-                        GPIO.output(pin, GPIO.HIGH)
-                    elif configuration["default_state"] == "default_off":
-                        GPIO.output(pin, GPIO.LOW)
-
-    def get_api_commands(self):
-        return dict(turnGpioOn=["id"], turnGpioOff=["id"], getGpioState=["id"])
+            pin = int(configuration["pin"])
+            # Example: creating an LED object for each pin
+            self.devices[pin] = LED(pin)  # Adjust for different types of devices
 
     def on_api_command(self, command, data):
-        if not user_permission.can():
-            return flask.make_response("Insufficient rights", 403)
+        pin = int(self._settings.get(["gpio_configurations"])[int(data["id"])]["pin"])
+        device = self.devices.get(pin)
 
-        configuration = self._settings.get(["gpio_configurations"])[int(data["id"])]
-        pin = self.get_pin_number(int(configuration["pin"]))
-
-        if command == "getGpioState":
-            if pin < 0:
-                return flask.jsonify("")
-            elif configuration["active_mode"] == "active_low":
-                return flask.jsonify("off" if GPIO.input(pin) else "on")
-            elif configuration["active_mode"] == "active_high":
-                return flask.jsonify("on" if GPIO.input(pin) else "off")
-        elif command == "turnGpioOn":
-            if pin > 0:
-                self._logger.info("Turned on GPIO{}".format(configuration["pin"]))
-
-                if configuration["active_mode"] == "active_low":
-                    GPIO.output(pin, GPIO.LOW)
-                elif configuration["active_mode"] == "active_high":
-                    GPIO.output(pin, GPIO.HIGH)
+        if command == "turnGpioOn":
+            device.on()
         elif command == "turnGpioOff":
-            if pin > 0:
-                self._logger.info("Turned off GPIO{}".format(configuration["pin"]))
-
-                if configuration["active_mode"] == "active_low":
-                    GPIO.output(pin, GPIO.HIGH)
-                elif configuration["active_mode"] == "active_high":
-                    GPIO.output(pin, GPIO.LOW)
-
-    def on_api_get(self, request):
-        states = []
-
-        for configuration in self._settings.get(["gpio_configurations"]):
-            pin = self.get_pin_number(int(configuration["pin"]))
-
-            if pin < 0:
-                states.append("")
-            elif configuration["active_mode"] == "active_low":
-                states.append("off" if GPIO.input(pin) else "on")
-            elif configuration["active_mode"] == "active_high":
-                states.append("on" if GPIO.input(pin) else "off")
-
-        return flask.jsonify(states)
+            device.off()
 
     def get_update_information(self):
         return dict(
@@ -193,22 +109,8 @@ class GpioControlPlugin(
             )
         )
 
-    PIN_MAPPINGS = [-1, -1, 3, 5, 7, 29, 31, 26, 24, 21, 19, 23, 32, 33, 8, 10, 36, 11, 12, 35, 38, 40, 15, 16, 18, 22, 37, 13]
-
-    def get_pin_number(self, pin):
-        if 2 <= pin <= 27:
-            if self.mode == GPIO.BCM:
-                return pin
-
-            if self.mode == GPIO.BOARD:
-                return self.PIN_MAPPINGS[pin]
-
-        return -1
-
-
 __plugin_name__ = "GPIO Control"
 __plugin_pythoncompat__ = ">=2.7,<4"
-
 
 def __plugin_load__():
     global __plugin_implementation__
